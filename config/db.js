07f1ec45a5457fda0cda557acbc29278e11da2db
Raj Ramesh Bengade <157134976+rajbengade07@users.noreply.github.com>
2025-02-26 16:07:35 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,1 +1,11 @@
+import mongoose from "mongoose";
+
+export const  connectDB = async () =>{
+
+    await mongoose.connect('mongodb+srv://rajbengade:raj010101@cluster0.sm60u.mongodb.net/').then(()=>console.log("DB Connected"));
+   
+}
+// mongodb+srv://rajbengade:mr__rajbengade@cluster0.shav7.mongodb.net/
+// add your mongoDB connection string above.
+// Do not use '@' symbol in your databse user's password else it will show an error
 
